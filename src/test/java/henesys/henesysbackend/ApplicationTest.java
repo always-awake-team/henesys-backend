d517818f940ee35838270d8dcbf1c557bc37eb3a
princeton <ssk0080@gmail.com>
2023-06-12 22:34:27 +0900

    backend setup

---
+++ src/test/java/henesys/henesysbackend/ApplicationTest.java
@@ -0,0 +1,13 @@
+package henesys.henesysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
