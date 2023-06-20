a8495ccf3859797d4459a1bed0e42de56f7537f8
princeton <ssk0080@gmail.com>
2023-06-20 14:16:41 +0900

    Feat: ArticleDetailDto 단건조회용 dto 생성

--- src/main/java/henesys/henesysbackend/article/domain/dto/ArticleDto.java
+++ src/main/java/henesys/henesysbackend/article/domain/dto/ArticleDto.java
@@ -3,13 +3,17 @@ package henesys.henesysbackend.article.domain.dto;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static henesys.henesysbackend.comment.domain.dto.CommentDto.*;
+import static lombok.AccessLevel.*;
 
 public class ArticleDto {
 
     @Getter
     @Builder
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = PROTECTED)
+    @AllArgsConstructor(access = PROTECTED)
     public static class ResponseArticleDto {
 
         private Long id;
@@ -21,6 +25,24 @@ public class ArticleDto {
         private int viewCount;
         private int likeCount;
         private LocalDateTime modifiedAt;
+
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = PROTECTED)
+    @AllArgsConstructor(access = PROTECTED)
+    public static class ResponseArticleDetailDto {
+
+        private Long id;
+        private String title;
+        private String author;
+        private String content;
+        private int commentCount;
+        private int viewCount;
+        private int likeCount;
+        private LocalDateTime modifiedAt;
+        private List<ResponseCommentDto> comments;
 
     }
 
