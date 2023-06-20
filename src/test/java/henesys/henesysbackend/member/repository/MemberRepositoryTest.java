5aabf967441e3d10e282f2158ab41b31fe5f0f96
princeton <ssk0080@gmail.com>
2023-06-20 15:00:35 +0900

    Refactor: MemberRepositoryTest
    테스트중 중복선언 제거, 사용하지 않는 어노테이션 제거, data jpa에서 지원하는 기능 테스트 제거

--- src/test/java/henesys/henesysbackend/member/repository/MemberRepositoryTest.java
+++ src/test/java/henesys/henesysbackend/member/repository/MemberRepositoryTest.java
@@ -2,7 +2,6 @@ package henesys.henesysbackend.member.repository;
 
 import henesys.henesysbackend.member.domain.entity.Member;
 import henesys.henesysbackend.member.domain.enumtype.RoleType;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
 @Transactional
-@Slf4j
 public class MemberRepositoryTest {
 
     @Autowired
@@ -25,58 +22,19 @@ public class MemberRepositoryTest {
 
     private Member memberA;
     private Member memberB;
+    private Member savedMember;
 
     @BeforeEach
     public void before() {
         memberA = new Member("memberA", "password", RoleType.USER, "nicknameA", "emailA");
         memberB = new Member("memberB", "password", RoleType.USER, "nicknameB", "emailB");
-    }
 
-    @Test
-    public void saveTest() throws Exception {
-        //when
-        Member savedMember = memberRepository.save(memberA);
-
-        //then
-        assertThat(savedMember.getId()).isEqualTo(memberA.getId());
-    }
-
-    @Test
-    public void findAllTest() throws Exception {
-        //given
-        memberRepository.save(memberA);
-        memberRepository.save(memberB);
-
-        //when
-        List<Member> findMembers = memberRepository.findAll();
-
-        //then
-        assertThat(findMembers.size()).isEqualTo(2);
-    }
-
-    @Test
-    public void findByIdTest() throws Exception {
-        //given
-        Member savedMember = memberRepository.save(memberA);
-        Long findId = savedMember.getId();
-        Long notExistsId = 238946L;
-
-        //when
-        Member findMember = memberRepository.findById(findId).get();
-        Optional<Member> notExistsMember = memberRepository.findById(notExistsId);
-
-        //then
-        assertThat(findMember.getName()).isEqualTo(savedMember.getName());
-        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
-
-        assertThat(notExistsMember).isEmpty();
+        savedMember = memberRepository.save(memberA);
     }
 
     @Test
     public void findAllByNameTest() throws Exception {
         //given
-        Member savedMember = memberRepository.save(memberA);
-
         Member duplicateNameMember = new Member("memberA", "password", RoleType.USER, "nicknameA", "emailA");
         memberRepository.save(duplicateNameMember);
 
@@ -94,14 +52,11 @@ public class MemberRepositoryTest {
     @Test
     public void findByNicknameTest() throws Exception {
         //given
-        Member savedMember = memberRepository.save(memberA);
         String findNickname = savedMember.getNickname();
-
         String notExistsNickname = memberB.getNickname();
 
         //when
         Member findMember = memberRepository.findByNickname(findNickname);
-
         Member notExistsMember = memberRepository.findByNickname(notExistsNickname);
 
         //then
