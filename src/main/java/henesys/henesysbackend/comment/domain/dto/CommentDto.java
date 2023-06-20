0321d0bcf8fa2b1fff91fa981a0464a43b718852
princeton <ssk0080@gmail.com>
2023-06-20 10:20:48 +0900

    Feat: comment dto 추가
    resqonseCommentDto 추가

---
+++ src/main/java/henesys/henesysbackend/comment/domain/dto/CommentDto.java
@@ -0,0 +1,25 @@
+package henesys.henesysbackend.comment.domain.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.*;
+
+public class CommentDto {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = PROTECTED)
+    @AllArgsConstructor(access = PROTECTED)
+    public static class ResponseCommentDto {
+
+        private Long id;
+        private Long authorId;
+        private String author;
+        private String content;
+        private LocalDateTime modifiedAt;
+
+    }
+
+}
